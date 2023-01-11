fb5ce4f7b32b9f0d9bb5ac3a4db9f6c8e570861d
Elias <eliasventura23@gmail.com>
2023-01-11 16:38:55 -0500

    updated dog model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,14 @@ class ApplicationController < Sinatra::Base
   get '/dogs/:id' do
     dog = Dog.find(params[:id])
     dog.to_json
-
   end
 
+  get '/walks' do
+    Walk.all.to_json
+  end
+
+  
+  
   get '/walks/:id' do
     walk = Walk.find(params[:id])
     walk.to_json
@@ -23,36 +28,51 @@ class ApplicationController < Sinatra::Base
 
   delete '/walks/:id' do
     walk = Walk.find(params[:id])
-    walk.delete
+    walk.destroy
+    walk.to_json
   end
 
   delete '/dogs/:id' do
     dog = Dog.find(params[:id])
-    dog.delete
+    dog.destroy
+    dog.to_json
   end
-
-
-  get '/walks' do
-    Walk.all.to_json
-  end
-
+  
   post '/dogs' do
-    
-    new_dog = Dog.create(params)
+    new_dog = Dog.create(
+      name: params[:name],
+      address: params[:address],
+      owner: params[:owner],
+      image: params[:image]
+      )
     new_dog.to_json
-    
   end
 
   post '/walks' do
-
-    new_walk = Walk.create(params)
+    new_walk = Walk.create(
+      dogwalker: params[:dogwalker],
+      starttime: params[:starttime],
+      length: params[:length],
+      dog_id: params[:dog_id]
+      )
     new_walk.to_json
   end
 
-  post '/walks/:id' do
-    binding.pry
-    updated_walk = Walk.update(params)
-    updated_walk
+  patch '/walks/:id' do
+    walk = Walk.find(params[:id])
+    walk.update(
+      dogwalker: params[:dogwalker],
+      starttime: params[:starttime],
+      length: params[:length])
+    walk.to_json
   end
+
+  get '/dogs/:id/walks' do
+    dog = Dog.find(params[:id])
+    walks = dog.walks
+    walks.to_json
+  end
+
+  
 
 end
--- app/models/Dog.rb
+++ app/models/Dog.rb
@@ -1,9 +1,3 @@
 class Dog < ActiveRecord::Base
-    has_many :walks
-
-    serialize :walks
-    
-    after_initialize do |b|
-        b.walks = [] if b.walks == nil
-    end
+    has_many :walks 
 end
