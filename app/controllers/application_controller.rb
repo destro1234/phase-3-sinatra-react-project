b7de06e4fe20d93d9f2a2e27a8c35d0c7ff56b60
Elias <eliasventura23@gmail.com>
2023-01-25 19:09:55 -0500

    minor changes to seeds and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,47 +6,37 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
-  get '/dogs' do
-    Dog.all.to_json(:include => :walks)
-    # :include => :posts)
+  #Create Dogs
+  post '/dogs' do
+    new_dog = Dog.create(
+      name: params[:name],
+      address: params[:address],
+      owner: params[:owner],
+      image: params[:image],
+      walks: []
+      )
+    new_dog.to_json
   end
 
+  #Read Dogs and Walks
+  get '/dogs' do
+    Dog.all.to_json(:include => :walks)
+  end
+
+  #Read Dogs
   get '/dogs/:id' do
     dog = Dog.find(params[:id])
     dog.to_json
   end
-
-  # get '/walks' do
-  #   Walk.all.to_json
-  # end
   
-  # get '/walks/:id' do
-  #   walk = Walk.find(params[:id])
-  #   walk.to_json
-  # end
-
-  delete '/walks/:id' do
-    walk = Walk.find(params[:id])
-    walk.destroy
-    walk.to_json
-  end
-
+  #Delete Dogs
   delete '/dogs/:id' do
     dog = Dog.find(params[:id])
     dog.destroy
     dog.to_json
   end
   
-  post '/dogs' do
-    new_dog = Dog.create(
-      name: params[:name],
-      address: params[:address],
-      owner: params[:owner],
-      image: params[:image]
-      )
-    new_dog.to_json
-  end
-
+  #Create Walks
   post '/walks' do
     new_walk = Walk.create(
       dogwalker: params[:dogwalker],
@@ -57,12 +47,26 @@ class ApplicationController < Sinatra::Base
     new_walk.to_json
   end
 
+  #Read Walks
+  get '/walks' do
+    Walk.all.to_json
+  end
+
+
+  #Update Walks
   patch '/walks/:id' do
     walk = Walk.find(params[:id])
     walk.update(
       dogwalker: params[:dogwalker],
       starttime: params[:starttime],
       length: params[:length])
+    walk.to_json
+  end
+
+  #Delete Walks
+  delete '/walks/:id' do
+    walk = Walk.find(params[:id])
+    walk.destroy
     walk.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ dog2 = Dog.create(name:"Luna", address: "Yonkers", image: "https://dl5zpyw5k3jeb
 dog3 = Dog.create(name: "Happy", address:"Manhattan", image: "https://hiddenpondlabradors.com/wp-content/uploads/2015/10/Dolce237.jpg")
 dog4 = Dog.create(name: "Biggs", address:"South Carolina", image:"https://www.thesprucepets.com/thmb/5ifyWuKUF7JaltzsQNy0vdFHJ84=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/breed_profile_germansheperd_1118000_profile_2608-d7a78e7c1cf049879bec1ec19113ee42.jpg")
 
-walk1 = Walk.create(dogwalker: "Max", starttime: "12:00 PM",  length: "15 minutes", dog_id: dog4.id)
-walk2 = Walk.create(dogwalker: "Nate", starttime: "10:00 PM",  length: "30 minutes", dog_id: dog1.id)
-walk3 = Walk.create(dogwalker: "Kevin", starttime: "12:00 PM",  length: "15 minutes", dog_id: dog3.id)
+walk1 = Walk.create(dogwalker: "Max", starttime: "12:00",  length: "15 minutes", dog_id: dog4.id)
+walk2 = Walk.create(dogwalker: "Nate", starttime: "10:00",  length: "30 minutes", dog_id: dog1.id)
+walk3 = Walk.create(dogwalker: "Kevin", starttime: "12:00",  length: "15 minutes", dog_id: dog3.id)
 
 
 
